42fe9570f93d6492d55e64d35846ded7b3a81640
Jason McFarlane <jmcfarlane.developer@gmail.com>
2024-09-01 15:48:52 +0400

    feat: add models

--- src/providers/mongo/mongo.provider.ts
+++ src/providers/mongo/mongo.provider.ts
@@ -1,6 +1,15 @@
+import { AuthenticationDatabaseProviderModule } from '@/modules/feature/authentication/authentication.provider'
+import { GameplayDatabaseProviderModule } from '@/modules/feature/gameplay/gameplay.provider'
+import { PlayersDatabaseProviderModule } from '@/modules/feature/players/players.provider'
+import { UsersDatabaseProviderModule } from '@/modules/feature/users/users.provider'
 import { Module } from '@nestjs/common'
 
 @Module({
-    imports: [],
+    imports: [
+        AuthenticationDatabaseProviderModule,
+        GameplayDatabaseProviderModule,
+        PlayersDatabaseProviderModule,
+        UsersDatabaseProviderModule,
+    ],
 })
 export class MongoDatabaseProviderModule {}
